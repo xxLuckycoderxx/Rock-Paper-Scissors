2cda0e70175ab9be3699872498575fd6b0a4348d
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-10-22 08:15:48 -0400

    beginning of project

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[]args){
+
+
+        String choice;
+        Scanner ape;
+        ape=new Scanner(System.in);
+
+        for(int name=1;name<4;name++) {
+            System.out.println("We are going to play a game of Rock Paper,Scissors.Ready set GO.Rock,Paper,Scissors...");
+            choice = ape.next.Lne();
+        }
+    }
+}
