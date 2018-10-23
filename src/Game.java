68daf9614c8e6e22c09a14e8c0f712b02c9ec4ef
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-10-23 08:12:25 -0400

    still  at the beginning of project

--- src/Game.java
+++ src/Game.java
@@ -5,12 +5,12 @@ public class Game {
 
 
         String choice;
-        Scanner ape;
-        ape=new Scanner(System.in);
+        Scanner keyboard;
+        keyboard=new Scanner(System.in);
 
         for(int name=1;name<4;name++) {
             System.out.println("We are going to play a game of Rock Paper,Scissors.Ready set GO.Rock,Paper,Scissors...");
-            choice = ape.next.Lne();
+            choice=keyboard.nextLine();
         }
     }
 }
