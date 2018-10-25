7668364361225099a1197ae58a68860014c4edbb
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-10-25 08:53:21 -0400

    no major changes to code

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[]args){
+         //use array to pull users choice\
+        //scor keeping variable with feedback of score to players
+        //loop to start game over
+        //use if else statementsto build game's core
+
+       /* int paper>rock+<scissors;
+        int scissors>paper+<rock;
+        int rock<scissors;
 
 
         String choice;
@@ -11,6 +19,17 @@ public class Game {
         for(int name=1;name<4;name++) {
             System.out.println("We are going to play a game of Rock Paper,Scissors.Ready set GO.Rock,Paper,Scissors...");
             choice=keyboard.nextLine();
-        }
+            if(choice==)
+        }*/
+
+      int sage[]=new int[140];
+      sage[4]=6;
+      sage[8]=10;
+
+      System.out.println(sage[4]+sage[8]);
+int magna[]={8,556,7,77};
+
+System.out.println(magna[2]);
+
     }
 }
