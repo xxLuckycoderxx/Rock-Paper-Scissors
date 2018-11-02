2bb5462707a3b1955dcfaa9eff434cf7eefa2d4a
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-11-02 08:16:42 -0400

    fixing changes to code

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -7,25 +8,13 @@ public class Game {
         //loop to start game over
         //use if else statementsto build game's core
 
-       /* int paper>rock+<scissors;
-        int scissors>paper+<rock;
-        int rock<scissors;
 
 
-        String choice;
-        Scanner keyboard;
-        keyboard=new Scanner(System.in);
-
-        for(int name=1;name<4;name++) {
-            System.out.println("We are going to play a game of Rock Paper,Scissors.Ready set GO.Rock,Paper,Scissors...");
-            choice=keyboard.nextLine();
-            if(choice==)
-        }*/
 
       int choices[]={0,1,2};
        String answer;
         Scanner keyboard=new Scanner(System.in);
-       int score=80;
+
 
        System.out.println("We are going to play a game of Rock,Paper,Scissor");
 
@@ -34,30 +23,34 @@ public class Game {
 
         System.out.println("Are you Ready?");
 
-            for(int mag=1;mag<4;mag++){
+
+            for(int mag=1;mag<2;mag++){
             System.out.println("Rock,Paper scissors,Shoot!!!(enter your answer below,Capitals)");
             answer=keyboard.nextLine();
+                int score=80;
+
+
+          if(answer.equals(0))
+              System.out.println("Try again ");
+          System.out.println(score=20);
+
+          else if(answer.equals(1))
+              System.out.println("Tie Game Over");
+              System.out.println(score-20);
+           break;
+
+          else if (answer.equals(2))
+              System.out.println("Winner");
+          System.out.println(score+20);
+              break;
+
+
+            }
 
 
 
 
-                    if(answer.equals(choices[0]))
-                    System.out.println("Try again ");
-                    System.out.println(score-20 +" points left");
 
-                    if(answer.equals(choices[1}))
-                    System.out.println("Pdd ");
-*/
-                     elseif(answer.equals(choices[1]))
-                     System.out.println("Nice Try");
-                     System.out.println(score-20+ "points left");
-
-             elseif(answer.equals(choices[2]))
-             System.out.println("GOOD JOB");
-             System.out.println(score+20+ "points left");
-
-
-        }
 
 
 
